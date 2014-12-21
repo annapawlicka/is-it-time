4e2c62ec6eb8a5053626dc23fddebcf9c3846a43
Anna Pawlicka <anna@mastodonc.com>
2014-12-21 19:25:35 +0100

    Upgrade cljs.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :source-paths ["src/clj" "src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2496" :scope "provided"]
+                 [org.clojure/clojurescript "0.0-2505" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
 
                  ;; Server
